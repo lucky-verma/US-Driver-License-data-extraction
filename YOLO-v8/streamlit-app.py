b427ec304c649f1406bc7a3751d7d090bbe63252
lucky verma <thinkersloop@gmail.com>
2023-05-01 02:37:11 -0400

    ocr processing and thresholding

--- YOLO-v8/streamlit-app.py
+++ YOLO-v8/streamlit-app.py
@@ -5,15 +5,26 @@ import time
 import numpy as np
 import easyocr
 from PIL import Image
-from parseq.strhub.data.module import SceneTextDataModule
 import torch
 from ultralytics import YOLO
 
-# Load model and image transforms
-parseq = torch.hub.load('baudm/parseq', 'parseq', pretrained=True).eval()
-img_transform = SceneTextDataModule.get_transform(parseq.hparams.img_size)
+# st configuration
+st.set_page_config(layout="wide",
+                   page_title="License",
+                   page_icon="🚗",
+                   initial_sidebar_state="expanded"
+                   )
 
-# Load model
+# hide made with streamlit
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Load object detection model
 model = YOLO("./new_models/best.pt")
 reader = easyocr.Reader(['en'], gpu=True)
 
@@ -24,18 +35,22 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    coll1, coll2 = st.columns(2)
+    coll1, coll2, col3 = st.columns(3)
     with coll1:
         st.image(image, caption='Uploaded Image.', use_column_width=True)
         st.write("")
-        st.info("Extracting text from image...")
+        # st loading bar
+        my_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percent_complete + 1)
 
     with coll2:
         # time the process
         start_time = time.time()
 
         # Run YOLOv8 model
-        results = model.predict(source=image, augment=True)
+        results = model.predict(source=image, augment=True, conf=0.60, iou=0.35)
 
         # Run OCR on each detected bounding box
         boxes = results[0].boxes
@@ -51,7 +66,7 @@ if uploaded_file is not None:
             cropped = image.crop((x1, y1, x2, y2))
             cropped = cropped.resize((cropped.width*3, cropped.height*3))
 
-            # add padding to cropped image 
+            # add padding to cropped image
             cropped = np.array(cropped)
             # cropped = cv2.copyMakeBorder(cropped, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[0, 0, 0])
             cropped = Image.fromarray(cropped)
@@ -59,33 +74,42 @@ if uploaded_file is not None:
             st.image(cropped, caption=f'{names[int(box.cls[0])]}', use_column_width=True)
 
             # run easyOCR on each cropped box and match with the class name
-            temp = reader.readtext(np.array(cropped), detail=0)
+            temp = reader.readtext(np.array(cropped),
+                                    detail=0,
+                                    # width_ths = 0.14,
+                                    allowlist='0123456789/-' if int(box.cls[0]) == 1 else None,
+                                    )
             if temp:
-                easy_results[names[int(box.cls[0])]] = [temp]
+                # Name processing
+                if int(box.cls[0]) == 2 and len(temp) > 3:
+                    temp.pop(0); temp.pop(1)
+                    easy_results[names[int(box.cls[0])]] = [temp]
 
-            # Run parseq model
-            logits = parseq(img_transform(cropped).unsqueeze(0))
+                # DOB processing
+                elif int(box.cls[0]) == 1:
+                    if len(temp) > 1:
+                        temp.pop(0); easy_results[names[int(box.cls[0])]] = [temp]
+                    else:
+                        temp = temp[0][-10:]
+                        easy_results[names[int(box.cls[0])]] = [temp]
 
-            # greedy decoding
-            pred = logits.softmax(-1)
-            label, confidence = parseq.tokenizer.decode(pred)
+                # State processing
+                elif int(box.cls[0]) == 3:
+                    temp = temp[-1] if len(temp) > 1 else temp
+                    easy_results[names[int(box.cls[0])]] = [temp]
 
-            if label:
-                parseq_results[names[int(box.cls[0])]] = [label]
-        
+                else:
+                    easy_results[names[int(box.cls[0])]] = [temp]
+
+    with col3:
+        # Display results from easyOCR
+        st.write("OCR Results")
+        st.write(easy_results)
+
     # Display time taken
-    st.write("Time taken: %s seconds" % (time.time() - start_time))
+    st.success("Time taken: %s seconds" % (time.time() - start_time))
 
-    # Display results in two columns
-    st.write("Results:")
-    col1, col2 = st.columns(2)
-    with col1: 
-        st.write("EasyOCR:")
-        st.json(easy_results)
-    with col2:
-        st.write("Parseq:")
-        st.json(parseq_results)
-
-
+    # display time and status  
+    print("Last parse: ", time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()))
 
 
